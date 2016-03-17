b8dafdf3d478be1b5cf6df82baee9b951f9f9164
Filip Grasheski <samonapominuvanje@hotmail.com>
2016-03-17 21:53:44 +0100

    Created ArticlesController for managing the published News and Services. Initial version works nicely. I need to add logic to display latest 3 news articles on the home page.

--- FamilyAuto/Controllers/HomeController.cs
+++ FamilyAuto/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FamilyAuto.Models;
 
 namespace FamilyAuto.Controllers
 {
     public class HomeController : Controller
     {
+        private FamilyAutoEntities db = new FamilyAutoEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var topNews = from n in db.Articles
+                          where n.ArticleType == 0
+                          select n;
+            topNews.Take(3).ToList();
+            return View(topNews);
         }
 
         public ActionResult About()
