bca65a45bb08f3231529a4a3a93584800eeb313c
Filip Grasheski <samonapominuvanje@hotmail.com>
2016-04-01 17:09:54 +0200

    Implemented a role system, still have to test it.

---
+++ FamilyAuto/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FamilyAuto.Models;
+
+namespace FamilyAuto.Controllers
+{
+    public class UserController : Controller
+    {
+        private FamilyAutoEntities db = new FamilyAutoEntities();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var customers = db.Customers.Include(c => c.AspNetUsers);
+            return View(customers.ToList());
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Phone,Email,Address,UserId")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", customers.UserId);
+            return View(customers);
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", customers.UserId);
+            return View(customers);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Phone,Email,Address,UserId")] Customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", customers.UserId);
+            return View(customers);
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customers customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customers customers = db.Customers.Find(id);
+            db.Customers.Remove(customers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
